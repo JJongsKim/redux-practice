324890c599d005a385d2df3c436fac6735237673
JJongsKim <totnwjd530@gmail.com>
2022-12-03 17:16:22 +0900

    UI용 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+
 function App() {
   return (
     <div className="App">
       <h1>리덕스 프로젝트 만들기</h1>
+      <Counter number={0} />
+      <br />
+      <br />
+      <Todos />
     </div>
   );
 }
