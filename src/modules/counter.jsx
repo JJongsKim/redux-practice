6a712f6832e684dd231efb9efc68334a978f9185
JJongsKim <totnwjd530@gmail.com>
2022-12-04 15:42:07 +0900

    redux-actions로 액션 생성 함수 만들기

--- src/modules/counter.jsx
+++ src/modules/counter.jsx
@@ -1,24 +1,33 @@
+// const INCREASE = 'counter/INCREASE';
+// const DECREASE = 'counter/DECREASE';
+
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+// const initialState = { number: 0 };
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default counter;
+
+// ------ 아래부터 redux-actions 사용 ------
+import { createAction } from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
-
-const initialState = { number: 0 };
-
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
-
-export default counter;
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
--- src/modules/todos.jsx
+++ src/modules/todos.jsx
@@ -1,77 +1,97 @@
+// const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
+// const INSERT = 'todos/INSERT'; // 새로운 todo 등록
+// const TOGGLE = 'todos/TOGGLE'; // todo 체크, 체크 해제
+// const REMOVE = 'todos/REMOVE'; // todo 제거
+
+// let id = 3; // todo 예제 객체 두 개를 만들 예정이라 다음에 추가될 항목의 id값을 넣어줌
+
+// export const changeInput = input => ({
+//   type: CHANGE_INPUT,
+//   input,
+// });
+
+// export const insert = text => ({
+//   type: INSERT,
+//   todo: {
+//     id: id++,
+//     text,
+//     done: false,
+//   },
+// });
+
+// export const toggle = id => ({
+//   type: TOGGLE,
+//   id,
+// });
+
+// export const remove = id => ({
+//   type: REMOVE,
+//   id,
+// });
+
+// const initialState = {
+//   input: '',
+//   todos: [
+//     {
+//       id: 1,
+//       text: '리덕스 기초 배우는중!',
+//       done: true,
+//     },
+//     {
+//       id: 2,
+//       text: '리액트에 리덕스 적용하기!',
+//       done: false,
+//     },
+//   ],
+// };
+
+// function todos(state = initialState, action) {
+//   switch (action.type) {
+//     case CHANGE_INPUT:
+//       return {
+//         ...state,
+//         input: action.input,
+//       };
+//     case INSERT:
+//       return {
+//         ...state,
+//         todos: state.todos.concat(action.todo),
+//       };
+//     case TOGGLE:
+//       return {
+//         ...state,
+//         todos: state.todos.map(todo =>
+//           todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+//         ),
+//       };
+//     case REMOVE:
+//       return {
+//         ...state,
+//         todos: state.todos.filter(todo => todo.id !== action.id),
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default todos;
+
+// ------ 아래부터 redux-actions 사용 ------
+import { createAction } from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo 등록
 const TOGGLE = 'todos/TOGGLE'; // todo 체크, 체크 해제
 const REMOVE = 'todos/REMOVE'; // todo 제거
 
-let id = 3; // todo 예제 객체 두 개를 만들 예정이라 다음에 추가될 항목의 id값을 넣어줌
+export const changeInpur = createAction(CHANGE_INPUT, input => input);
 
-export const changeInput = input => ({
-  type: CHANGE_INPUT,
-  input,
-});
+let id = 3;
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
 
-export const insert = text => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
-});
-
-export const toggle = id => ({
-  type: TOGGLE,
-  id,
-});
-
-export const remove = id => ({
-  type: REMOVE,
-  id,
-});
-
-const initialState = {
-  input: '',
-  todos: [
-    {
-      id: 1,
-      text: '리덕스 기초 배우는중!',
-      done: true,
-    },
-    {
-      id: 2,
-      text: '리액트에 리덕스 적용하기!',
-      done: false,
-    },
-  ],
-};
-
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map(todo =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id),
-      };
-    default:
-      return state;
-  }
-}
-
-export default todos;
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
