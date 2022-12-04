df4dcfc64f6f7a7850d3842b22196d31c00256c1
JJongsKim <totnwjd530@gmail.com>
2022-12-04 16:34:58 +0900

    useDispatch로 액션 디스패치, useCallback으로 성능 최적화

--- src/containers/CounterContainer.jsx
+++ src/containers/CounterContainer.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import Counter from '../components/Counter';
+// import { increase, decrease } from '../modules/counter';
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+// };
+
+// const mapStateToProps = state => ({
+//   number: state.counter.number,
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+// ------ 아래부터 Hooks 사용 ------
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 };
 
-const mapStateToProps = state => ({
-  number: state.counter.number,
-});
-
-const mapDispatchToProps = dispatch => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+export default CounterContainer;
